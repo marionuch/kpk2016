7ad26c30d7f81236b61449a2c2308440a56e279a
marionuch <marionuch@yandex.ru>
2016-08-27 00:16:13 +0500

    Попытка рисования координатной сетки

--- draw.py
+++ draw.py
@@ -4,14 +4,16 @@ def init_drawman():
     global t, x_current, y_current, _drawman_scale
     t=Turtle()
     t.penup()
+    drawman_scale(default_scale)
     x_current=0
     y_current=0
     t.goto(x_current,y_current)
-    drawman_scale(default_scale)
 
 def drawman_scale(scale):
     global _drawman_scale
     _drawman_scale=scale
+    grid(_drawman_scale)
+
 def test_drawman():
     """
     Тестирование работы Чертёжника
@@ -38,9 +40,59 @@ def to_point(x,y):
     y_current=y
     t.goto(_drawman_scale*x,_drawman_scale*y)
 
+def coordinates():
+    t.goto(0,0)
+    t.down()
+    t.goto(0,300)
+    t.goto(0,-300)
+    t.goto(0,0)
+    t.goto(350,0)
+    t.goto(-350,0)
+    t.penup()
+
+def grid(scale):
+    t.color("gray")
+    t.goto(0,300)
+    k=350//scale//2
+    x=k
+    for i in range(k):
+        t.goto(x,300)
+        t.pendown()
+        t.goto(x,-300)
+        x+=k
+        t.penup()
+    x=k
+    for i in range(k):
+        t.goto(x,300)
+        t.pendown()
+        t.goto(x,-300)
+        x-=k
+        t.penup()
+    k=300//scale//2
+    x=k
+    for i in range(k):
+        t.goto(350,x)
+        t.pendown()
+        t.goto(-350,x)
+        x+=k
+        t.penup()
+    x=k
+    for i in range(k):
+        t.goto(350,x)
+        t.pendown()
+        t.goto(-350,x)
+        x-=k
+        t.penup()
+    t.penup()
+    t.color("black")
+    coordinates()
+
+
+
+
 init_drawman()
 if __name__=='__main__':
     import time
     test_drawman()
-    time.sleep(10)
+    #time.sleep(10)
 
